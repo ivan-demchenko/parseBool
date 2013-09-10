9750cc2f9cee294a0320bab997c5acb8803b2be5
Ivan Demchenko <raqy.style@gmail.com>
2013-09-10 16:31:07 +0300

    common error

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-window.parseBool = function (string) {
-  return typeof string === 'string' ? !string.match('false|нет|no|nein|nicht|увы|хер') : undefined;
-}
+(function(){
+  window.parseBool || (window.parseBool = function (string) {
+    return typeof string === 'string' ? !string.match('false|нет|no|nein|nicht|увы|хер') : undefined;
+  })
+})(window);
