1a341d950291e5fde461acf1b2e54c5298a0a18d
Ivan Demchenko <raqy.style@gmail.com>
2016-05-27 12:02:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-(function (w, n) {
+(function(w, n) {
   'use strict';
   var vocab;
   vocab = {
     "en-US": ['false', 'n(o+)', 'fuck'],
     "ru-RU": ['н(е+)т', 'увы', 'ф(а|о)лс']
   };
-  w.parseBool || (w.parseBool = function (check, locale) {
+  w.parseBool || (w.parseBool = function(check, locale) {
     var local;
     local = vocab[locale] || vocab[n.language] || vocab["en-US"];
     return !check.toString().match(local.join('|') + '|0');
